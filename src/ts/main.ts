5404b7f071632b0efa52f6df90009e2ccb01ca4f
Sergei Kudlai <s.kudlai@mail.ru>
2022-12-18 14:32:10 +0300

    fix: Error ts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,62 +1,70 @@
-let myDropdownCategory = document.querySelector(".dropbtn-cat") as HTMLElement;
-let dropdownContent = document.querySelector(".dropdown-active") as HTMLElement;
+const myDropdownCategory = document.querySelector('.dropbtn-cat');
+const dropdownContent = document.querySelector('.dropdown-active');
 
-let myDropdownRecoment = document.querySelector(".dropbtn-recomend") as HTMLElement;
-let dropdownRecomend = document.querySelector(".dropdown-active-rec") as HTMLElement;
+const myDropdownRecoment = document.querySelector('.dropbtn-recomend');
+const dropdownRecomend = document.querySelector('.dropdown-active-rec');
 
-let myDropdownPrice = document.querySelector(".dropbtn-price") as HTMLElement;
-let dropdownPrice = document.querySelector(".dropdown-active-price") as HTMLElement;
+const myDropdownPrice = document.querySelector('.dropbtn-price');
+const dropdownPrice = document.querySelector('.dropdown-active-price');
 
-myDropdownCategory.addEventListener('click', () => {
-    dropdownContent.classList.toggle("show");
-  })
+if (myDropdownCategory && dropdownContent) {
+  myDropdownCategory.addEventListener('click', () => {
+    dropdownContent.classList.toggle('show');
+  });
+}
 
-myDropdownRecoment.addEventListener('click', () => {
-    dropdownRecomend.classList.toggle("show");
-  })
+if (myDropdownRecoment && dropdownRecomend) {
+  myDropdownRecoment.addEventListener('click', () => {
+    dropdownRecomend.classList.toggle('show');
+  });
+}
 
+if (myDropdownPrice && dropdownPrice) {
   myDropdownPrice.addEventListener('click', () => {
-    dropdownPrice.classList.toggle("show");
-  })
-  
-  const rangeInput = document.querySelectorAll(".range-input input") as NodeListOf<Element>;
-  const priceInput = document.querySelectorAll(".price-input input") as NodeListOf<HTMLInputElement>;
-  const range = document.querySelector(".slider .progress") as HTMLElement;
-  let priceGap = 1000;
+    dropdownPrice.classList.toggle('show');
+  });
+}
 
-// priceInput.forEach((input) => {
-//   input.addEventListener("input", (e: any): void => {
-//     let minPrice: number = parseInt(priceInput[0].value),
-//       maxPrice: number = parseInt(priceInput[1].value);
+const rangeInput = document.querySelectorAll('.range-input input'); //as NodeListOf<Element>;
+const priceInput = document.querySelectorAll('.price-input input');
+const range = document.querySelector('.slider .progress') as HTMLElement;
+const priceGap = 1000;
 
-//     if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
-//       if (e.target.className === "input-min") {
-//         rangeInput[0].value = minPrice;
-//         range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
-//       } else {
-//         rangeInput[1].value = maxPrice;
-//         range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
-//       }
-//     }
-//   });
-// });
+priceInput.forEach((input) => {
+  input.addEventListener('input', (e: Event): void => {
+    const minPrice = Number((priceInput[0] as HTMLInputElement).value),
+      maxPrice = Number((priceInput[1] as HTMLInputElement).value);
 
-// rangeInput.forEach((input: any) => {
-//   input.addEventListener("input", (e) => {
-//     let minVal: number = parseInt(rangeInput[0].value),
-//       maxVal: number = parseInt(rangeInput[1].value);
+    if (maxPrice - minPrice >= priceGap && maxPrice <= Number(rangeInput[1].getAttribute('max'))) {
+      if ((e.target as HTMLElement).className === 'input-min') {
+        (rangeInput[0] as HTMLInputElement).value = minPrice.toString();
+        range.style.left = (minPrice / Number(rangeInput[0].getAttribute('max'))) * 100 + '%';
+      } else {
+        (rangeInput[1] as HTMLInputElement).value = String(minPrice);
+        if (range) {
+          range.style.right = 100 - (maxPrice / Number(rangeInput[1].getAttribute('max'))) * 100 + '%';
+        }
+      }
+    }
+  });
+});
 
-//     if (maxVal - minVal < priceGap) {
-//       if (e.target.className === "range-min") {
-//         rangeInput[0].value = maxVal - priceGap;
-//       } else {
-//         rangeInput[1].value = minVal + priceGap;
-//       }
-//     } else {
-//       (priceInput[0].value) = minVal;
-//       priceInput[1].value = maxVal;
-//       range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
-//       range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
-//     }
-//   });
-// });
+rangeInput.forEach((input) => {
+  input.addEventListener('input', (e: Event) => {
+    const minVal = Number((rangeInput[0] as HTMLInputElement).value),
+      maxVal = Number((rangeInput[1] as HTMLInputElement).value);
+
+    if (maxVal - minVal < priceGap) {
+      if ((e.target as HTMLElement).className === 'range-min') {
+        (rangeInput[0] as HTMLInputElement).value = (maxVal - priceGap).toString();
+      } else {
+        (rangeInput[1] as HTMLInputElement).value = (minVal + priceGap).toString();
+      }
+    } else {
+      (priceInput[0] as HTMLInputElement).value = minVal.toString();
+      (priceInput[1] as HTMLInputElement).value = maxVal.toString();
+      range.style.left = (minVal / Number(rangeInput[0].getAttribute('max'))) * 100 + '%';
+      range.style.right = 100 - (maxVal / Number(rangeInput[1].getAttribute('max'))) * 100 + '%';
+    }
+  });
+});
